65c06b6a9245460ab9219b8b87f2f5b827a964ab
Forceur <francois.besnard13@gmail.com>
2024-01-10 10:31:41 +0100

    Try to fix

--- R/0_Download & install the function.R
+++ R/0_Download & install the function.R
@@ -1,23 +1,38 @@
-#  Just to when you want to update the package ====
-
-#Install & Load devtools package which will connect to GitHub to download the package I did
-#install.packages("devtools")
-library(devtools)
-
-#Install the package I did from GitHub
-devtools::install_github("FBesnard13/francoisfct-1")
-
-
-#  Do everytime you want to use my functions in your scripts ====
-
-#Load my function in your scripts
-library(francoisfct)
-
-
-#  Tips & Tricks ====
-
-#Run this to see the list of functions I created
-help(package = 'francoisfct')
-
-#Run this to get more information avout a function (change the function name if needed)
-?f_boxplot
+# #  Just do when you want to update the package ====
+#
+# #Install & Load devtools package which will connect to GitHub to download the package I did
+# #install.packages("devtools")
+# library(devtools)
+#
+# #Install the package I did from GitHub
+# devtools::install_github("FBesnard13/francoisfct-1")
+#
+#
+# #  IF IT DOESN'T WORK: Install localy: ====
+#
+# #Install & Load devtools package which will connect to GitHub to download the package I did
+# #install.packages("devtools")
+# library(devtools)
+#
+# #Copy Paste the URL in your navigator
+# #https://api.github.com/repos/FBesnard13/francoisfct-1/tarball/HEAD
+#
+# #Replace the following code by the URL of the downloaded file
+# #devtools::install_local(URL TO CHANGE: r"(C:\Users\fbesnard\Downloads\FBesnard13-francoisfct-1-3746c72.tar.gz)")
+#
+#
+#
+#
+# #  Do everytime you want to use my functions in your scripts ====
+#
+# #Load my function in your scripts
+# library(francoisfct)
+#
+#
+# #  Tips & Tricks ====
+#
+# #Run this to see the list of functions I created
+# help(package = 'francoisfct')
+#
+# #Run this to get more information avout a function (change the function name if needed)
+# ?f_boxplot
