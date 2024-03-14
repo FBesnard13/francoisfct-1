95e405f73c0bccd56796f6daae72a4b8db20a68a
FBesnard13 <francois.besnard13@gmail.com>
2024-03-14 19:13:35 +0100

    test to fix bug

--- R/0_Use Rprofile.R
+++ R/0_Use Rprofile.R
@@ -1,21 +1,21 @@
-# Run this to open .Rprofile ====
-
-home_directory <- path.expand("~")
-rprofile_path <- file.path(home_directory, ".Rprofile")
-file.edit(rprofile_path)
-
-
-# Write the following in it & save the file ====
-
-# suppressMessages(library(francoisfct))
-# suppressMessages(library(tidyverse))
-# suppressMessages(library(readxl))
-# suppressMessages(library(openxlsx))
-# suppressMessages(library(arrow))
-# suppressMessages(library(scales))
-# suppressWarnings(suppressMessages(library(conflicted)))
-# suppressWarnings(suppressMessages(conflict_prefer("filter", "dplyr")))
-
-
-
-# Restard R ====
+# # Run this to open .Rprofile ====
+#
+# home_directory <- path.expand("~")
+# rprofile_path <- file.path(home_directory, ".Rprofile")
+# file.edit(rprofile_path)
+#
+#
+# # Write the following in it & save the file ====
+#
+# # suppressMessages(library(francoisfct))
+# # suppressMessages(library(tidyverse))
+# # suppressMessages(library(readxl))
+# # suppressMessages(library(openxlsx))
+# # suppressMessages(library(arrow))
+# # suppressMessages(library(scales))
+# # suppressWarnings(suppressMessages(library(conflicted)))
+# # suppressWarnings(suppressMessages(conflict_prefer("filter", "dplyr")))
+#
+#
+#
+# # Restard R ====
