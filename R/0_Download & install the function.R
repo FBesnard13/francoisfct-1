8ab1c2151f4d9d3ffb60c66cf8bfe89ac761d842
Fbesnard <francois.besnard13@gmail.com>
2024-04-17 12:24:20 +0200

    Update 0_Download & install the function.R

--- R/0_Download & install the function.R
+++ R/0_Download & install the function.R
@@ -5,11 +5,11 @@
 # #Copy Paste the URL in your navigator
 # #https://api.github.com/repos/FBesnard13/francoisfct-1/tarball/HEAD
 #
-# Detach the package first (unload becacuse of .RProfile load)
+# # Detach the package first (unload becacuse of .RProfile load)
 # detach("package:francoisfct", unload = TRUE)
 #
 # #Replace the following code by the URL of the downloaded file
-# devtools::install_local(r"(C:\Users\fbesnard\Downloads\FBesnard13-francoisfct-1-1d9281c.tar.gz)")
+# devtools::install_local(r"(C:\Users\fbesnard\Downloads\FBesnard13-francoisfct-1-54ae1c5.tar.gz)")
 #
 #
 #
@@ -27,4 +27,4 @@
 #
 # #Run this to get more information avout a function (change the function name if needed)
 # ?f_boxplot
-
+#
